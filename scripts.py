5a60e0a723055d605f6c38b0b1fee7d9fa49143f
Real1ty <vavro.matej3@gmail.com>
2024-08-18 17:40:24 +0200

    feat: added scripts.py containg scripts that can be run through poetry

---
+++ scripts.py
@@ -0,0 +1,9 @@
+import subprocess
+
+
+def format_code():
+    subprocess.run(["black", "--line-length", "120", "."], check=True)
+
+
+def lint_code():
+    subprocess.run(["pylint", "--max-line-length=120", "notion_apilib"], check=True)
